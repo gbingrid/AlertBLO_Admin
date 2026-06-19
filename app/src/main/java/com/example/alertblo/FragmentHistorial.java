48876cb55d63a44af19ba2f2643ba5ad5ba40376
Ingridgb <ingrid_gabu99@outlook.com>
2026-06-19 21:10:54 +0200

    Configuración de themes y botón salir para FragmentHistorial

--- app/src/main/java/com/example/alertblo/FragmentHistorial.java
+++ app/src/main/java/com/example/alertblo/FragmentHistorial.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,35 +13,65 @@ import com.google.android.material.chip.Chip;
 
 public class FragmentHistorial extends Fragment{
 
+    private Chip todas, critica, aviso;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
 
         View view = inflater.inflate(R.layout.fragment_historial, container, false);
 
-        Chip todas = view.findViewById(R.id.chipTodas);
-        Chip critica = view.findViewById(R.id.chipAlertaCritica);
-        Chip aviso = view.findViewById(R.id.chipAlertaNormal);
+        todas = view.findViewById(R.id.chipTodas);
+        critica = view.findViewById(R.id.chipAlertaCritica);
+        aviso = view.findViewById(R.id.chipAlertaNormal);
 
         if(todas != null){
             todas.setOnClickListener(v -> {
+                actualizarEstadosFiltros(todas);
                 if(MainActivity.adaptador != null) MainActivity.adaptador.filtrarAlerta("todas");
             });
         }
 
         if(critica != null){
             critica.setOnClickListener(v -> {
+                actualizarEstadosFiltros(critica);
                 if(MainActivity.adaptador != null) MainActivity.adaptador.filtrarAlerta("critica");
             });
         }
 
         if(aviso != null){
             aviso.setOnClickListener(v -> {
+                actualizarEstadosFiltros(aviso);
                 if(MainActivity.adaptador != null) MainActivity.adaptador.filtrarAlerta("aviso");
             });
         }
 
+        if(todas != null){
+            actualizarEstadosFiltros(todas);
+        }
+
+        // Configuración del botón Salir
+        ImageButton btnSalir = view.findViewById(R.id.btn_salir);
+
+        // Al hacer click, se llama al método público creado en MainActivity
+        btnSalir.setOnClickListener(v -> {
+            if (getActivity() instanceof MainActivity) {
+                ((MainActivity) getActivity()).mostrarDialogoSalirPublico();
+            }
+        });
         return view;
+    }
+
+    // Actualizar estado de los filtros
+    private void actualizarEstadosFiltros(Chip chipActivo) {
+        if (todas == null || critica == null || aviso == null) return;
+
+        todas.setChecked(false);
+        critica.setChecked(false);
+        aviso.setChecked(false);
+
+        chipActivo.setChecked(true);
+
     }
 
     @Override
